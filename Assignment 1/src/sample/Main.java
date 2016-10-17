d5e2c5f37942a8f214eb5cc6f95b9b43ffbf0501
SG1995 <0898958@hr.nl>
2016-10-17 14:26:56 +0200

    added files

---
+++ Assignment 1/src/sample/Main.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("screen.fxml"));
+        primaryStage.setTitle("Assignment 1");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args)
+    {
+
+        Model model = new Model();
+        model.connect();
+        model.insertTest();
+
+        launch(args);
+
+    }
+}
