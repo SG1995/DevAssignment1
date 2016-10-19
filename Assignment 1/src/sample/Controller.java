ced63deb66b2e4a3eef0e68fffd64ec67262cb6d
SG1995 <0898958@hr.nl>
2016-10-19 13:42:52 +0200

    //WIP

--- Assignment 1/src/sample/Controller.java
+++ Assignment 1/src/sample/Controller.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Observable;
 
+import static java.lang.Integer.getInteger;
 import static java.lang.Integer.parseInt;
 
 public class Controller
@@ -66,6 +67,26 @@ public class Controller
         }
     }
 
+    public void AddProjectView(ActionEvent actionEvent)
+    {
+        try{
+            //load the fxml in fxmlloader.
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddProjectView.fxml"));
+            //Place fxml in Parent.
+            Parent root1 = (Parent) fxmlLoader.load();
+            //Create Stage and (might) edit stage attributes
+            stage = new Stage();
+            stage.setTitle("AddProject");
+            // pick parent to set on stage and show.
+            stage.setScene(new Scene(root1));
+            stage.show();
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 
 
 
@@ -109,6 +130,19 @@ public class Controller
         System.out.println("Done");
     }
 
+    @FXML private TextField P_ID;
+    @FXML private TextField Budget;
+    @FXML private TextField Total_Amount_Of_Allocated_Hours;
+    @FXML private TextField Building_Name;
 
+    public void OnAddProjectClick(ActionEvent actionEvent)
+    {
+        int P_ID = parseInt(this.P_ID.getText());
+        float Budget = Float.parseFloat(this.Budget.getText());
+        float Total_Amount_Of_Allocated_Hours = Float.parseFloat(this.Total_Amount_Of_Allocated_Hours.getText());
+        String Building_Name = this.Building_Name.getText();
+
+
+    }
 
 }
